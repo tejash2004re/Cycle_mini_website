3086ea8dd13a3505d89879b620c85355b874e904
TEJASHWIN S <144519865+tejash2004re@users.noreply.github.com>
2024-06-07 21:09:18 +0530

    Add files via upload

---
+++ Cycle_website/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+  $("#hello").mouseenter(function(){
+      $("#he").show("slow" );
+    });
+    $("#hello").mouseleave(function(){
+      $("#he").hide("slow" );
+    });
+    $("#hello1").mouseenter(function(){
+      $("#he1").show( "slow");
+    });
+    $("#hello1").mouseleave(function(){
+      $("#he1").hide( "slow");
+    })
+    $("#hello2").mouseenter(function(){
+      $("#he2").show("slow" );
+    });
+    $("#hello2").mouseleave(function(){
+      $("#he2").hide("slow" );
+    });
+    $("#hello3").mouseenter(function(){
+      $("#he3").show( "slow");
+    });
+    $("#hello3").mouseleave(function(){
+      $("#he3").hide( "slow");
+    });
+  });
+
+
